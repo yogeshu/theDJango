ac080117b1290f83d167b3ed4868fa721e612bd6
yogeshu <yogeshbhavsar1994@gmail.com>
2019-04-11 12:25:06 +0530

    fist commit in rest in simple

---
+++ CustomerList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import CustomersService from './djangoprox/frontend/src/CustomersService';
+const customersServcie = new CustomersService
+class CustomersList extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default CustomersList;
